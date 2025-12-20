cb14ea6988ee7acc052f1addcbabe29c4738ff7f
abhinn14 <abhinnprakashyadav@gmail.com>
2025-12-20 10:50:42 +0530

    fixed bugs

--- backend/user-service/src/middleware/auth.ts
+++ backend/user-service/src/middleware/auth.ts
@@ -1,8 +1,12 @@
 import type { NextFunction, Request, Response } from "express";
 import jwt, { type JwtPayload } from "jsonwebtoken";
-import User from "../models/user.js";
+import type { IUser } from "../models/user.js";
 
-export const isAuth = async (req:Request,res:Response,next:NextFunction):Promise<void> => {
+export interface AuthReq extends Request{
+    user?: IUser | null;
+}
+
+export const isAuth = async (req:AuthReq,res:Response,next:NextFunction):Promise<void> => {
     try {
         const authHeader = req.headers.authorization;
         if(!authHeader || !authHeader.startsWith("Bearer ")) {
@@ -11,11 +15,25 @@ export const isAuth = async (req:Request,res:Response,next:NextFunction):Promise
         }
         
         const token = authHeader.split(" ")[1];
-        const decodeValue = jwt.verify(token,process.env.JWT_KEY as string) as JwtPayload;
-         
+        if(!token) {
+            res.status(401).json({ message: "Token missing" });
+            return;
+        }
+        const JWT_KEY = process.env.JWT_KEY;
+        if(!JWT_KEY) {
+          throw new Error("JWT_KEY not defined");
+        }
+        const decodeValue = jwt.verify(token,JWT_KEY) as JwtPayload;
+        if(!decodeValue || !decodeValue.user) {
+            res.status(401).json({message:"Invalid token"});
+            return;
+        }
 
+        req.user = decodeValue.user;
+        next();
     } catch(error) {
-        console.log("JWT error => ",error);
-        res.status(401).json({message:"JWT error"});
+        console.log("JWT verify error => ",error);
+        res.status(401).json({message:"JWT verify error"});
     }
 }
+
