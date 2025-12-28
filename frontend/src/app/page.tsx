c34246a99a0e369eb12e625c2ffddd3349c9b707
abhinn14 <abhinnprakashyadav@gmail.com>
2025-12-29 01:44:22 +0530

    fixed bugs

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -19,10 +19,17 @@ const HomeContent = () => {
         {blogLoading ? (
           <PageLoading />
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
-                             xl:grid-cols-5
-                             2xl:grid-cols-6
-                            gap-12">
+          <div
+  className="
+    grid
+    grid-cols-1
+    sm:grid-cols-2
+    md:grid-cols-3
+    lg:grid-cols-4
+    gap-8
+  "
+>
+
             {blogs?.length === 0 && <p>No Blogs Yet</p>}
             {blogs?.map((e, i) => (
               <BlogCard
